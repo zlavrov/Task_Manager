02b55e3e8ecae3953e2bbdd0196e80a09eb00d5c
@zlavrov <zakhar.polosky@gmail.com>
2022-05-30 19:41:15 +0300

    Task_Manager

---
+++ taskmanager/main/forms.py
@@ -0,0 +1,19 @@
+from .models import Task
+from django.forms import ModelForm, TextInput, Textarea
+
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'task']
+
+
+        widgets = {
+            'title':TextInput(attrs={
+                'class':'form-control',
+                'placeholder':'Enter the title'
+            }),
+            'task':Textarea(attrs={
+                'class':'form-control',
+                'placeholder':'Enter a description'
+                }),
+        }
